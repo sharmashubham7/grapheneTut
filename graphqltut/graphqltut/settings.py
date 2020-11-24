0781b46ecb9702694faf2519a411574c6371803b
sharmashubham7 <49106036+sharmashubham7@users.noreply.github.com>
2020-11-24 17:56:44 -0500

    started django project added graphene lib created graphene endpoint

--- graphqltut/graphqltut/settings.py
+++ graphqltut/graphqltut/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'ingredients',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# GRAPHENE = {'SCHEMA':'graphqltut.schema.schema'}
